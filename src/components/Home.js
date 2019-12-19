179fc0ab47858795c9b4d8dc5ee69f018455569a
psycho33200 <pininfarina47@gmail.com>
2019-12-19 14:02:47 +0100

    add content home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="title">MIND SPACE</div>
+      <div className="regles">
+        Officia esse eiusmod sit officia tempor dolor aliquip enim ea ex minim
+        aliquip aute. Duis enim nostrud fugiat voluptate deserunt sint in dolor.
+        Velit adipisicing do nostrud aliquip. Sint ut dolor commodo elit
+        consectetur Lorem non occaecat sint occaecat et velit labore do. Aliqua
+        tempor dolore laborum magna nulla irure quis culpa. Dolore velit culpa
+        tempor nulla culpa.
+      </div>
+      <div className="button">
+        <Link to="/game">
+          <button>PLAY</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
